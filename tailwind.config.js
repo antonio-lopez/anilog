3035ab664bc09a4fb69da935a2b136209f35a024
Antonio Lopez <pezlop202@gmail.com>
2022-11-12 15:10:38 -0800

    Initial commit w/ Storybook and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './stories/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
